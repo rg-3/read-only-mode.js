40ff36009c7837fb50daa5ff6b2858ec761cddd7
rg <1aab@protonmail.com>
2020-12-19 08:48:03 +0100

    extend `stylesheet` to set display: none; on selector [data-read-only-mode="1"]

--- src/js/content-scripts/content-script.js
+++ src/js/content-scripts/content-script.js
@@ -9,28 +9,32 @@ const stylesheet = `
     margin-top: 15px;
     justify-content: center;
   }
+
+  [data-read-only-mode="1"] {
+    display: none !important;
+  }
 `;
 
-const setDisplayNone = (els) => {
+const tagElements = (els) => {
   for(let i = 0; i < els.length; i++) {
     const el = els[i];
     if(!el || el.getAttribute('data-read-only-mode') === '1') { continue; }
     el.setAttribute('data-read-only-mode', '1');
-    el.style.display = 'none';
   }
 };
 
 chrome.runtime.sendMessage("IsEnabled", (isEnabled) => {
   if(!isEnabled) { return; }
+
   setInterval(() => {
     /* Remove the compose area */
-    setDisplayNone([document.querySelector('div[data-testid="primaryColumn"] div[role="progressbar"]')?.parentElement]);
+    tagElements([document.querySelector('div[data-testid="primaryColumn"] div[role="progressbar"]')?.parentElement]);
     /* Remove the compose tweet button */
-    setDisplayNone(document.querySelectorAll('a:not([data-read-only-mode="1"])[href="/compose/tweet"]'));
+    tagElements(document.querySelectorAll('a:not([data-read-only-mode="1"])[href="/compose/tweet"]'));
     /* Remove reply icon */
-    setDisplayNone(document.querySelectorAll('div:not([data-read-only-mode="1"])[data-testid="reply"]'));
+    tagElements(document.querySelectorAll('div:not([data-read-only-mode="1"])[data-testid="reply"]'));
     /* Remove retweet icon */
-    setDisplayNone(document.querySelectorAll('div:not([data-read-only-mode="1"])[data-testid="retweet"]'));
+    tagElements(document.querySelectorAll('div:not([data-read-only-mode="1"])[data-testid="retweet"]'));
   }, 250);
 
   const el = document.createElement('style');
