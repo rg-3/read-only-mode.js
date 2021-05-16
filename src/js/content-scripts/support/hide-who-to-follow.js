fe3e7dbda2d08f017b14e0fc2d9cad21f977cfad
rg <1aab@protonmail.com>
2021-05-16 13:32:41 -0300

    extract header removal into a function

--- src/js/content-scripts/support/hide-who-to-follow.js
+++ src/js/content-scripts/support/hide-who-to-follow.js
@@ -1,18 +1,32 @@
 import toggleHide from '../support/toggle-hide.js';
 
-export default function(disabled) {
-  let spans = Array.from(document.querySelectorAll('span'));
-  spans
-    .filter((el) => ['Who to follow'].includes(el.textContent))
-    .forEach((el) => {
-      let target = el;
+const ignoredPaths = [
+  '/followers', '/following', '/likes', '/retweets', '/timeline'
+];
+
+const headerText = [
+  'Who to follow'
+];
+
+const removeHeader = (disabled) => {
+  const spans = document.querySelectorAll('span');
+  for(let i = 0; i < spans.length; i++) {
+    const span = spans[i];
+    if(headerText.includes(span.textContent)) {
+      let target = span;
       let targetDepth = 5;
       for(let i = 0; i < targetDepth; i++) {
         toggleHide([target], {when: disabled.includes('whoToFollow'), tag: 'display-none'})
         target = target.parentElement;
       }
-     });
-  if(['followers', 'following', 'likes', 'retweets'].filter((path) => document.location.href.endsWith(path)).length === 0) {
+    }
+  }
+};
+
+export default function(disabled) {
+  removeHeader(disabled);
+  /* Skip other contexts where we don't want to remove UserCell nodes */
+  if(!ignoredPaths.filter((path) => document.location.href.endsWith(path)).length) {
     toggleHide('div[data-testid=UserCell]', {when: disabled.includes('whoToFollow'), tag: 'display-none'});
   }
   toggleHide([document.querySelector('aside[aria-label="Who to follow"]')?.parentElement], {when: disabled.includes('whoToFollow'), tag: 'display-none'});
