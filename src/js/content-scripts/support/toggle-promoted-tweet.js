b3ff9318693c338654aa5a17b478287edfaabf02
rg <1aab@protonmail.com>
2021-05-20 10:31:08 -0300

    update toggle-promoted-tweet.js

--- src/js/content-scripts/support/toggle-promoted-tweet.js
+++ src/js/content-scripts/support/toggle-promoted-tweet.js
@@ -1,21 +1,15 @@
-import toggle from '../support/toggle.js';
+import toggle from './toggle.js';
 
 export default function (disabled) {
-  const spans = Array.from(document.querySelectorAll('span'));
-  spans
-    .filter((el) => el.textContent === 'Promoted')
-    .forEach((el) => {
-      toggle([el], disabled.includes('promotedTweet'), 'display-none');
-      toggle([el.closest('div[data-testid=tweet')], disabled.includes('promotedTweet'), 'display-none');
-    });
-  spans
-    .filter((el) => el.textContent === 'Promoted Tweet')
-    .forEach((el) => {
-      let target = el;
-      const targetDepth = 5;
-      for (let i = 0; i < targetDepth; i++) {
-        toggle([target], disabled.includes('promotedTweet'), 'display-none');
-        target = target.parentElement;
+  const spans = document.querySelectorAll('span');
+  for (let i = 0; i < spans.length; i++) {
+    const el = spans[i];
+    if (el.textContent === 'Promoted' || el.textContent.startsWith('Promoted by')) {
+      const tweet = el.closest('div[data-testid=tweet');
+      if(tweet) {
+        toggle([el], disabled.includes('promotedTweet'), 'display-none');
+        toggle([tweet], disabled.includes('promotedTweet'), 'display-none');
       }
-    });
+    }
+  }
 }
