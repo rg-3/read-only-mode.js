a41d215a82de1473468c48bdd1902b3a2dc6d079
rg <1aab@protonmail.com>
2021-05-24 20:16:54 -0300

    describe as strategies instead of tags

--- src/js/content-scripts/content-script-helpers/toggle.js
+++ src/js/content-scripts/content-script-helpers/toggle.js
@@ -1,4 +1,4 @@
-const tags = {
+const strategies = {
   'scale-zero': 'data-twitter-mod-scale-zero',
   'display-none': 'data-twitter-mod-display-none'
 };
@@ -9,21 +9,21 @@ const tags = {
   When the second argument is true a hide takes place.
   When the second argument is false a show takes place.
 */
-export default function (els, hidden, tag = 'scale-zero') {
-  tag = tags[tag];
+export default function (els, hidden, strategy= 'scale-zero') {
+  strategy = strategies[strategy];
   if (hidden) {
     for (let i = 0; i < els.length; i++) {
       const el = els[i];
-      if (!el || el.getAttribute(tag) === '1') { continue; }
+      if (!el || el.getAttribute(strategy) === '1') { continue; }
       el.setAttribute('data-twitter-mod', '1');
-      el.setAttribute(tag, '1');
+      el.setAttribute(strategy, '1');
     }
   } else {
     for (let i = 0; i < els.length; i++) {
       const el = els[i];
       if (!el) { continue; }
       el.removeAttribute('data-twitter-mod');
-      for (const k in tags) { el.removeAttribute(tags[k]); }
+      for (const key in strategies) { el.removeAttribute(strategies[key]); }
     }
   }
 }
